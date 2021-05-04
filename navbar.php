42e6628a5421a8b63d48c5dcf2472dc617fff2b6
n1szycode <n1szycode@gmail.com>
2021-05-04 21:43:44 +0700

    update index

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e6ccb2; color: #7f5539;">
   <div class="container">
-    <a class="navbar-brand" href="index.php">Gudang</a>
+    <a class="navbar-brand" href="index.php">Gudang Buku</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
